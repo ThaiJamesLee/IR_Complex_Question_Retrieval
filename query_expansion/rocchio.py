5b41be18a886751227a870e5a229786f55b01ac4
routerhan <he5540192@gmail.com>
2019-04-30 22:55:04 +0200

    Update rocchio.py

--- query_expansion/rocchio.py
+++ query_expansion/rocchio.py
@@ -29,7 +29,7 @@ class RocchioOptimizeQuery:
     def get_initial_weight(self, matrix):
         weights = {}
         for doc_id in matrix.keys():
-            doc = matrix[str(doc_id)]
+            doc = matrix[doc_id]
             for term in list(doc.keys()):
                 weights[term] = 0.0
         return weights
@@ -37,7 +37,7 @@ class RocchioOptimizeQuery:
     def get_doc_weights(self, docs):
         doc_weights = {}
         for doc_id in docs:
-            doc = self.matrix[str(doc_id)]
+            doc = self.matrix[doc_id]
             for term in list(doc.keys()):
                 if term in doc_weights:
                     doc_weights[term] = doc_weights[term] + doc[term]
@@ -45,7 +45,9 @@ class RocchioOptimizeQuery:
                     doc_weights[term] = doc[term]
         return doc_weights
 
-    def execute_rocchio(self, relevant_docs, non_relevant_docs):
+    # number_new_terms : amount of terms wanted to be added into initial query
+    # return : a new query vector which is origin query vector with additional added new terms vector.
+    def execute_rocchio(self, relevant_docs, non_relevant_docs, number_new_terms):
         weights = self.weights
         relevant_docs_weights = self.get_doc_weights(relevant_docs)
         non_relevant_docs_weights = self.get_doc_weights(non_relevant_docs)
@@ -57,16 +59,26 @@ class RocchioOptimizeQuery:
             else:
                 weights[term] = weights[term] + self.beta * (relevant_docs_weights[term] / len(relevant_docs))
 
-        # Compute new query vector, *to do : save the positive weight for getting new terms
+        # Compute new query vector
+        filtered = {}
         for term in weights.keys():
             if term in self.query_vector.keys():
                 self.new_query_vector[term] = self.alpha * self.query_vector[term] + weights[term]
+                filtered.update({term: self.new_query_vector[term]})
             elif weights[term] > 0:
                 self.new_terms[term] = weights[term]
                 self.new_query_vector[term] = weights[term]
-        return self.new_query_vector
 
-    # additional terms added based on the Rocchio
+        index = 0
+        for w in sorted(self.new_terms, key=self.new_terms.get, reverse=True):
+            if index < number_new_terms: #e.g. add only 2 new terms into initial query vector
+                filtered.update({w: self.new_terms[w]})
+                index += 1
+            else:
+                break
+        return filtered
+
+    # return : additional terms vector based on the Rocchio
     def get_topk_new_terms(self, top_k):
         filtered = {}
         index = 0
