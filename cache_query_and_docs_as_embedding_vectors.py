c64f20a4aa3b5f1a0d8ba18695796188a67b7ace
Duc Tai Ly <ductaily@yahoo.de>
2019-05-04 22:08:13 +0200

    #22 delete processed_data, process_data is new meta

--- cache_query_and_docs_as_embedding_vectors.py
+++ cache_query_and_docs_as_embedding_vectors.py
@@ -14,7 +14,7 @@ from performance import Performance
 class Caching:
     """
     Prerequisites:
-    if there are no processed_data or cache directories or they are empty
+    if there are no process_data or cache directories or they are empty
     - you ran preprocessing at least once
     - you ran cache_word_embeddings at least once
     """
@@ -27,12 +27,12 @@ class Caching:
         """
         self.process_type = process_type
         if process_type == 'lemma':
-            self.corpus = pickle.load(open('processed_data/lemma_processed_paragraph.pkl', 'rb'))
-            self.queries = pickle.load(open('processed_data/lemma_processed_query.pkl', 'rb'))
+            self.corpus = pickle.load(open('process_data/lemma_process_paragraph.pkl', 'rb'))
+            self.queries = pickle.load(open('process_data/lemma_process_query.pkl', 'rb'))
             self.tf_idf_cache = 'cache/lemma_tf_idf.pkl'
         elif process_type == 'stem':
-            self.corpus = pickle.load(open('processed_data/processed_paragraph.pkl', 'rb'))
-            self.queries = pickle.load(open('processed_data/processed_query.pkl', 'rb'))
+            self.corpus = pickle.load(open('process_data/process_paragraph.pkl', 'rb'))
+            self.queries = pickle.load(open('process_data/process_query.pkl', 'rb'))
             self.tf_idf_cache = 'cache/tf_idf.pkl'
 
         self.cached_embeddings = pickle.load(open('cache/word_embedding_vectors.pkl', 'rb'))
@@ -43,11 +43,11 @@ class Caching:
         self.vector_dimension = vector_dimension
 
         # create queries
-        self.raw_query = pickle.load(open('processed_data/raw_query.pkl', 'rb'))
+        self.raw_query = pickle.load(open('process_data/raw_query.pkl', 'rb'))
         # self.query_map = Caching.create_query_map(self.raw_query, self.process_type)
-        self.test = pickle.load(open('processed_data/simulated_test.pkl', 'rb'))
+        self.test = pickle.load(open('process_data/simulated_test.pkl', 'rb'))
         # self.unique_doc = np.unique(list(self.test['docid']))
-        self.paragraph_ids = pickle.load(open('processed_data/paragraph_ids.pkl', 'rb'))
+        self.paragraph_ids = pickle.load(open('process_data/paragraph_ids.pkl', 'rb'))
         self.doc_structure = self.create_document_corpus()
 
         # cache file paths
@@ -73,7 +73,7 @@ class Caching:
     def create_document_corpus(self):
         """
         Creates the term-document count matrix.
-        It takes lemma_processed_paragraphs.pkl as input.
+        It takes lemma_process_paragraphs.pkl as input.
         :return: Returns a dict of {docid: {term: value, ...}, ...}
         """
         print('create document corpus as dict...')
@@ -108,7 +108,7 @@ class Caching:
                 sum_embedding_vectors[idx] = sum_embedding_vectors[idx] / sum_weight
 
             query_embedding_vector.update({q: sum_embedding_vectors})
-        # avg_query_embeddings.pkl contains {processed_query: nparray, ...}
+        # avg_query_embeddings.pkl contains {process_query: nparray, ...}
         pickle.dump(query_embedding_vector, open(self.avg_query_embeddings, 'wb'))
 
     def create_query_embeddings_query_expansion(self, top_k=10):
@@ -149,7 +149,7 @@ class Caching:
                 sum_embedding_vectors[idx] = sum_embedding_vectors[idx] / sum_weight
 
             query_embedding_vector.update({q: sum_embedding_vectors})
-        # avg_query_embeddings.pkl contains {processed_query: nparray, ...}
+        # avg_query_embeddings.pkl contains {process_query: nparray, ...}
         pickle.dump(query_embedding_vector, open(self.avg_query_expanded_embeddings, 'wb'))
         return query_embedding_vector
 
